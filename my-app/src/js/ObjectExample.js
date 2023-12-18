73b38ceaed2f71749623aa92aedf29fd2cc712d9
hiiiiiilim <vnfwlq07@naver.com>
2023-12-18 17:27:16 +0900

    .

--- my-app/src/js/ObjectExample.js
+++ my-app/src/js/ObjectExample.js
@@ -5,7 +5,7 @@ function ObjectExample(){
     const updateAge = () => {
         //...person : 현재 person 객체 속성을 펼쳐서 새로운 객체로 만들어주는 역할을 함
         //객체 속성들은 그대로 복사가 되서 만들어짐
-
+        // '20' : 문자열과 숫자를 더하는 것으로 인식 ex) 20+1=201
         //age : person.age + 1 age속성에 person.age+1한 값으로 age 값을 저장하겠다.
         //...person, age , 의 역할은 복사된 객체에서 값이 변화하거나 추가해야할 내용이 있으면 , 뒤에 값과 속성을 추가함
         setPerson({...person, age : person.age + 1});
